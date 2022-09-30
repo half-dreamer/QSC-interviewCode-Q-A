cfec7944ee26f9d8c7f14d9868f56739971d03d6
half-dreamer <zy1319097537@gmail.com>
2022-09-30 21:37:51 +0800

    Q&A 1.7

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,26 @@
+// 修改部分QWQ
 let myImage = document.querySelector("img");
+let button1 = document.getElementById("Q1");
+let button2 = document.getElementById("Q2");
+let button3 = document.getElementById("Q3");
+let button4 = document.getElementById("Q4");
+let button5 = document.getElementById("Q5");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let c1 = document.getElementById("c1");
+let a1 = document.getElementById("a1");
+const questions = ["Java和JavaScript的关系","长者的名言是什么","open the door of coding","世界一流退学","别碰我QWQ"];
+button1.textContent = questions[0];
+button2.textContent = questions[1];
+button3.textContent = questions[2];
+button4.textContent = questions[3];
+button5.textContent = questions[4];
+p1.textContent = "可以点击图片哦😇"
+p2.textContent = "too young,too simple,sometime naive"
+p3.textContent = ">>> print(\"hello world!\") \n hello world!"
+a1.textContent = "www.世界一流退学.com"
+//修改部分末QWQ
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
